74a2dcc6beeb36a15bf8caf46b815a25cecf90c4
reeenanana <renahownewbee@gmail.com>
2026-05-06 17:10:39 +0800

    fixture

--- testcases/test_pytest_m.py
+++ testcases/test_pytest_m.py
@@ -11,24 +11,34 @@ from config.driver_config import DriverConfig
 
 
 class TestPytestMClass:
+
+    @pytest.fixture(scope='class')
+    def scope_class(self):
+        print("我是class级别，我只执行一次")
+
+    @pytest.fixture(scope='function')
+    def driver(self):
+        get_driver = DriverConfig().driver_config()
+        return get_driver
+
     @pytest.mark.bilibili
-    def test_open_bilibili(self):
-        driver = DriverConfig().driver_config()
+    def test_open_bilibili(self, driver, scope_class):
+        # driver = DriverConfig().driver_config()
         driver.get("https://www.bilibili.com/")
         sleep(3)
         driver.quit()
 
     @pytest.mark.baidu
-    def test_open_baidu(self):
+    def test_open_baidu(self, driver, scope_class):
         print("test_open_baidu")
-        driver = DriverConfig().driver_config()
+        # driver = DriverConfig().driver_config()
         driver.get("https://www.baidu.com")
         sleep(3)
         driver.quit()
 
     @pytest.mark.google
-    def test_open_google(self):
-        driver = DriverConfig().driver_config()
+    def test_open_google(self, driver, scope_class):
+        # driver = DriverConfig().driver_config()
         driver.get("https://www.google.com")
         sleep(3)
         driver.quit()
