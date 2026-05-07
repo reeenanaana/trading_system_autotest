22b876a7d4665ca6dcd18070be783150267bceb7
reeenanana <renahownewbee@gmail.com>
2026-05-07 12:18:23 +0800

    失败重跑插件pytest-rerunfailures

---
+++ testcases/test_rerun.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+# coding = utf-8
+# @Time: 2026/5/7 11:46
+# @Author: Rena
+
+import random
+
+import pytest
+
+
+class TestRerun:
+    @pytest.mark.flaky(reruns=5, reruns_delay=1)
+    def test_rerun(self):
+        num = random.randint(1, 3)
+        print(num)
+        if num != 1:
+            print("失败")
+            raise Exception("出错了")
+        else:
+            print("成功")
+
+# class TestRerun:
+#     # 在命令行添加reruns / reruns-delay参数可以对全局用例做设置
+#     # pytest -s testcases/test_rerun.py --reruns 5 --reruns-delay 1
+#     def test_rerun(self):
+#         num = random.randint(1, 3)
+#         print(num)
+#         if num != 1:
+#             print("失败")
+#             raise Exception("出错了")
+#         else:
+#             print("成功")
