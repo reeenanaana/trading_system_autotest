44d2bcad6cf5c0f067f20040e97987ff8b237c01
reeenanana <renahownewbee@gmail.com>
2026-05-12 12:22:35 +0800

    test_jenkins

---
+++ testcases/test_jenkins.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+# coding = utf-8
+# @Time: 2026/5/12 12:21
+# @Author: Rena
+import unittest
+
+
+class TestJenkins:
+    def test_jenkins(self, driver):
+        print("Testing Jenkins")
